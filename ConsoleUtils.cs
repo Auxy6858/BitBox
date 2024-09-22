a018959acee954b48c3417313fdb010c2a8eefb3
Auxy6858 <71662994+Auxy6858@users.noreply.github.com>
2024-09-23 00:12:18 +0100

    Initial commit

---
+++ ConsoleUtils.cs
@@ -0,0 +1,17 @@
+﻿namespace BitBox;
+
+public static class ConsoleUtils
+{
+    public static string? Input(string? value) 
+    {
+        Console.Write(value);
+        return Console.ReadLine();
+    }
+
+    public static string? InputLine(string? value)
+    {
+        Console.WriteLine(value);
+        return Console.ReadLine();
+    }
+}
+
---
+++ StringUtils.cs
@@ -0,0 +1,18 @@
+﻿namespace BitBox
+{
+    public static class StringUtils
+    {
+        public static string? Reverse(string value)
+        {
+            int arrayLen = value.Length;
+            char[] reversedString = new char[arrayLen];
+            
+            for (int i = 0; i < arrayLen; i++)
+            {
+                reversedString[i] = value[arrayLen - 1 - i];
+            }
+            
+            return new string(reversedString);
+        }
+    }
+}
